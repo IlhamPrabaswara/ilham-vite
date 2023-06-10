f304fa7c9adf95c8ea9081c72245b90dcdb3a8b2
Ilham Prabaswara <ilham.prabaswara@ai.astra.co.id>
2023-06-10 07:45:41 +0700

    Reinstall react-vite

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-vite
-Personal portfolio with Vite
--- src/Routes.js
+++
@@ -1,6 +0,0 @@
-<Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
