0d40aa3658f38d64126344b858528a5efa0db751
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-06-24 00:21:46 +0700

    fix(src): Remove eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,5 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    "indent": ["error", 2],
-    "quotes": ["error", "single"]
   },
 }
