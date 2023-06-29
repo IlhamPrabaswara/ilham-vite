0dfb41c334156740ab9690d892207408d142b7f8
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-06-29 19:11:05 +0700

    feat(pages/contact): Integrate form with supabase db

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,44 +1,128 @@
+import { useState } from 'react'
 import {
   Button,
   FormControl,
   Input,
+  // Modal,
+  // ModalBody,
+  // ModalCloseButton,
+  // ModalContent,
+  // ModalFooter,
+  // ModalHeader,
+  // ModalOverlay,
   Stack,
   Text,
   Textarea,
+  useDisclosure,
 } from '@chakra-ui/react'
 import PageContainer from '../../layouts/pageContainer'
+import { supabase } from '../../utils/supabaseClient'
+import { ContactFormType } from '../../interfaces/contact.interfaces'
 
 const Contact = () => {
-  const contact = {
-    first: 'Ilham',
-    last: 'Prabaswara',
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [input, setInput] = useState<ContactFormType>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message: '',
+  })
+  const handleChange = (e: any) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    const { data } = await supabase
+      .from('messages')
+      .insert({
+        firstName: input.firstName,
+        lastName: input.lastName,
+        email: input.email,
+        message: input.message,
+      })
+      .select()
+
+    if (data) {
+      setInput({
+        ...input,
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+      })
+      alert("Your message is successfully sent!")
+      // return (
+      //   <Modal isOpen={isOpen} onClose={onClose}>
+      //     <ModalOverlay />
+      //     <ModalContent>
+      //       <ModalHeader>Modal Title</ModalHeader>
+      //       <ModalCloseButton />
+      //       <ModalBody>
+      //         <h1>Hello</h1>
+      //       </ModalBody>
+
+      //       <ModalFooter>
+      //         <Button colorScheme="blue" mr={3} onClick={onClose}>
+      //           Close
+      //         </Button>
+      //         <Button variant="ghost">Secondary Action</Button>
+      //       </ModalFooter>
+      //     </ModalContent>
+      //   </Modal>
+      // )
+    }
   }
   return (
     <PageContainer>
       <div id="contact">
         <h1>
-          {contact.first || contact.last ? (
-            // <h1>
-            //   {contact.first} {contact.last}
-            // </h1>
-            <>
-              <Text mb={'50px'}>
-                Got a project you'd like to discuss? I'm all ears.
-              </Text>
+          <>
+            <Text mb={'50px'}>
+              Got a project you'd like to discuss? I'm all ears.
+            </Text>
+            <form onSubmit={handleSubmit}>
               <FormControl>
                 <Stack gap={'30px'}>
-                  <Input placeholder="Name" />
-                  <Input placeholder="Email" />
-                  <Textarea placeholder="Message"></Textarea>
-                  <Button variant={'outline'} w={'full'}>
+                  <Input
+                    name="firstName"
+                    value={input.firstName}
+                    placeholder="First Name"
+                    onChange={handleChange}
+                  />
+                  <Input
+                    name="lastName"
+                    value={input.lastName}
+                    placeholder="Last Name"
+                    onChange={handleChange}
+                  />
+                  <Input
+                    name="email"
+                    value={input.email}
+                    placeholder="Email"
+                    onChange={handleChange}
+                  />
+                  <Textarea
+                    name="message"
+                    value={input.message}
+                    placeholder="Message"
+                    onChange={handleChange}
+                  />
+                  <Button
+                    type="submit"
+                    variant={'outline'}
+                    w={'full'}
+                    onClick={onOpen}
+                    onChange={handleChange}
+                  >
                     Submit
                   </Button>
                 </Stack>
               </FormControl>
-            </>
-          ) : (
-            <i>No Name</i>
-          )}
+            </form>
+          </>
         </h1>
       </div>
     </PageContainer>
