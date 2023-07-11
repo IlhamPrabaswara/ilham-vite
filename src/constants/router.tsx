dd7af567973a2c6262e96a833ebb1855e6106195
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-07-11 23:16:01 +0700

    feat(src/constants): Add pages to router

--- src/constants/router.tsx
+++ src/constants/router.tsx
@@ -2,17 +2,25 @@ import { createBrowserRouter } from 'react-router-dom'
 import Root from '../pages/Root'
 import ErrorPage from '../error-page'
 import Contact from '../pages/Contact'
+import About from '../pages/About'
+import Blog from '../pages/Blog'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+  {
+    path: 'blog',
+    element: <Blog />,
+  },
+  {
+    path: 'contact',
+    element: <Contact />,
   },
 ])
