6b10341dde897ddcaa4e3f915ec223ed2a85873c
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-07-16 21:56:32 +0700

    feat: Add jest config

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+// };
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
+
+export default config
