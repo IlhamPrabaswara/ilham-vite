25e286096b37d193e023101951850b177e292d03
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-07-19 10:27:47 +0700

    feat (pages/contact): Add new contact form

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,10 +1,120 @@
+import { useEffect } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
 import MainLayout from '../../layout'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Text,
+  Textarea,
+} from '@chakra-ui/react'
+import { supabase } from '../../utils/supabaseClient'
+import { ContactFormType } from '../../interfaces/contact.interfaces'
 
 const Contact = () => {
+  const { control, handleSubmit, reset, formState } = useForm<ContactFormType>({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+    },
+  })
+  const onSubmit: SubmitHandler<ContactFormType> = async (dataMessage) => {
+    const { data }: any = await supabase
+      .from('messages')
+      .insert({
+        firstName: dataMessage.firstName,
+        lastName: dataMessage.lastName,
+        email: dataMessage.email,
+        message: dataMessage.message,
+      })
+      .select()
+    if (data) {
+      alert(`Hi ${data[0].firstName}! your message is successfully sent!`)
+    }
+    // Try catch
+    // Absolute import untuk vite
+  }
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset({
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+      })
+    }
+  }, [formState])
   return (
     <MainLayout>
-      <h1>Contact Page</h1>
-      <p>This is contact page</p>
+      <div id="contact">
+        <h1>
+          <>
+            <Text mb={'50px'}>
+              Got a project you&apos;d like to discuss? I&apos;m all ears.
+            </Text>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack gap={'30px'}>
+                <Controller
+                  name="firstName"
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <>
+                      <FormControl>
+                        <FormLabel>First Name</FormLabel>
+                        <Input
+                          placeholder="Your first name (required)"
+                          {...field}
+                        />
+                      </FormControl>
+                    </>
+                  )}
+                />
+                <Controller
+                  name="lastName"
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Last Name</FormLabel>
+                      <Input
+                        placeholder="Your last name (required)"
+                        {...field}
+                      />
+                    </FormControl>
+                  )}
+                />
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Email</FormLabel>
+                      <Input placeholder="Your email (required)" {...field} />
+                    </FormControl>
+                  )}
+                />
+                <Controller
+                  name="message"
+                  control={control}
+                  render={({ field }) => (
+                    <FormControl>
+                      <FormLabel>Message</FormLabel>
+                      <Textarea placeholder="Message" {...field} />
+                    </FormControl>
+                  )}
+                />
+                <Button type="submit" variant={'outline'} w={'full'}>
+                  Submit
+                </Button>
+              </Stack>
+            </form>
+          </>
+        </h1>
+      </div>
     </MainLayout>
   )
 }
