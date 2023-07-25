8037e2279b5a044c387c145236a6da66fb6e8e07
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-07-25 20:13:09 +0700

    fix: Change layout default font and padding value

--- src/layout.tsx
+++ src/layout.tsx
@@ -7,7 +7,9 @@ export default function MainLayout({
 }) {
   return (
     <>
-      <Box backgroundColor={'neutral.95'} padding={4}>{children}</Box>
+      <Box backgroundColor={'neutral.95'} fontFamily={'Inter'} px={4} py={5}>
+        {children}
+      </Box>
     </>
   )
 }
