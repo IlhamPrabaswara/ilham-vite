f25235ba684c178d9fa3479a8deb712d268e8d05
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2023-07-25 20:14:29 +0700

    fix(components): Change nav bar style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,67 +1,111 @@
 import { FC } from 'react'
 import {
+  Box,
   Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
+  Collapse,
   Flex,
   Icon,
+  Stack,
   useDisclosure,
 } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
 const NavBar: FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onToggle } = useDisclosure()
   return (
-    <Flex
-      alignItems={'center'}
-      justifyContent={'space-between'}
-      bg="tomato"
-      w="100%"
-      paddingX={4}
-      paddingY={3}
-      color="white"
+    <Box
+      className="navbar"
+      display={'block'}
+      h={12}
+      w={'full'}
+      position={'sticky'}
+      top={'0px'}
+      zIndex={9999}
     >
-      <Link to={'/'}>Ilham</Link>
-      <Button onClick={onOpen} variant={'link'}>
-        <Icon
-          width="6"
-          height="6"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+      <Box
+        className="ac-ln-wrapper"
+        position={'absolute'}
+        w={'full'}
+        h={'100%'}
+        top={0}
+        zIndex={1}
+      >
+        <Box
+          className="ac-ln-background"
+          display={'block'}
+          h={isOpen ? '100vh' : '100%'}
+          position={'absolute'}
+          width={'full'}
+          zIndex={1}
+          bg={'rgba(255, 255, 255, 0.9)'}
+          backdropFilter={'blur(20px)'}
+          _after={{
+            content: '""',
+            backgroundColor: 'rgba(0,0,0,0.16)',
+            display: 'block',
+            height: '1px',
+            position: 'absolute',
+            top: '100%',
+            width: '100vw',
+          }}
+        ></Box>
+        <Flex
+          className="ac-ln-content"
+          align={'center'}
+          h={12}
+          justify={'space-between'}
+          mb={5}
+          px={4}
+          position={'relative'}
+          top={0}
+          zIndex={2}
         >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z"
-            fill="#ffffff"
-          />
-        </Icon>
-      </Button>
-      <Drawer placement="top" isOpen={isOpen} onClose={onClose} size={'full'}>
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader p={2} height={'48px'} borderBottomWidth="1px">
-            Menu
-          </DrawerHeader>
-          <DrawerBody>
-            <Flex
-              direction={'column'}
-              gap={3}
-              position={'absolute'}
-              width={'full'}
+          <Link to={'/'}>Ilham</Link>
+          <Button
+            aria-label="Open navigation"
+            onClick={onToggle}
+            variant={'link'}
+          >
+            <Icon
+              width="6"
+              height="6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z"
+                fill="#45464F"
+              />
+            </Icon>
+          </Button>
+          <Box
+            as={Collapse}
+            in={isOpen}
+            position={'absolute'}
+            top={12}
+            endingHeight={'calc(100vh - 48px)'}
+            zIndex={9999}
+          >
+            <Stack
+              onClick={onToggle}
+              h="calc(100vh - 48px)"
+              w={'100vw'}
+              px={8}
+              mt={4}
+              spacing={15}
+              zIndex={9999}
             >
               <Link to={'/about'}>About</Link>
               <Link to={'/blog'}>Blog</Link>
               <Link to={'/contact'}>Contact</Link>
-            </Flex>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </Flex>
+            </Stack>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 
